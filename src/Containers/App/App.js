f2450b8bc339da86e1b40752fb04a98b642ad757
willydelerue <willy.delerue@gmail.com>
2023-11-06 09:52:07 +0100

    Projete to-do list en react

---
+++ src/Containers/App/App.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import classes from './App.module.css';
+import Task from '../../Components/Task/Task';
+
+function App() {
+
+  // States
+  const [tasks, setTasks] = useState([]);
+  const [input, setInput] = useState('');
+
+  // Fonctions
+  const removeClickedHandler = index => {
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  }
+
+  const doneClickedHandler = index => {
+    const newTasks = [...tasks];
+    newTasks[index].done = !tasks[index].done;
+    setTasks(newTasks);
+  }
+
+  const submittedTaskHandler = event => {
+    event.preventDefault();
+
+    const newTask = {
+      content: input,
+      done: false
+    }
+    setTasks([...tasks, newTask]);
+    setInput('');
+  }
+
+  const changedFormHandler = event => {
+    setInput(event.target.value);
+  }
+
+  // Variables
+  let tasksDisplayed = tasks.map((task, index) => (
+    <Task
+      done={task.done}
+      content={task.content}
+      key={index}
+      removeClicked={() => removeClickedHandler(index)}
+      doneClicked={() => doneClickedHandler(index)}
+    />
+  ));
+  // let donedTasks = tasks.filter(task => task.done)
+  //   .map((filteredTask, index) => (
+  //     <Task
+  //       done={filteredTask.done}
+  //       content={filteredTask.content}
+  //       key={index}
+  //       removeClicked={() => removeClickedHandler(index)}
+  //       doneClicked={() => doneClickedHandler(index)}
+  //     />
+  // ));
+
+  return (
+    <div className={classes.App}>
+      <header>
+        <span>TO-DO</span>
+      </header>
+
+      <div className={classes.add}>
+        <form onSubmit={(e) => submittedTaskHandler(e)}>
+          <input
+            type="text"
+            value={input}
+            onChange={(e) => changedFormHandler(e)}
+            placeholder="Que souhaitez-vous ajouter ?" />
+          <button type="submit">
+            Ajouter
+          </button>
+        </form>
+      </div>
+      {tasksDisplayed}
+    </div>
+  );
+}
+
+export default App;
